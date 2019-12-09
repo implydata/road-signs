a80082db259bffefcc76b90abbc73c754b9c41f6
Sébastien <dolor_sit_amet@protonmail.ch>
2019-12-09 14:25:14 -0800

    added demo

---
+++ src/road-sign/road-sign.demo.tsx
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019 Imply Data, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Imply Data, Inc.
+ */
+
+import { RoadSign } from '@implydata/caladan';
+import { ApiDoc, Gallery, SideBySide, Sink } from 'hear-ye';
+import * as React from 'react';
+
+function Demo() {
+  return (
+    <SideBySide>
+      <Sink>
+        <RoadSign />
+      </Sink>
+      <ApiDoc interfaceName="RoadSign" />
+    </SideBySide>
+  );
+}
+
+Gallery.add({ path: ['RoadSign'], component: <Demo /> });
